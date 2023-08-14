3c1585f23b7199eb90ccd4706ec010d26df39d84
Marcel Bartholet <Marcel.Bartholet@web.de>
2023-08-14 23:42:28 +0200

    add metadata for resource pack creation

--- MultiVersionModDevTool.Common/Models/Environment/ResourcePack.cs
+++ MultiVersionModDevTool.Common/Models/Environment/ResourcePack.cs
@@ -3,6 +3,9 @@ namespace de.z0rdak.moddev.util.Models.Environment;
 public class ResourcePack
 {
     public string Dir { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
     public bool CreateAfterFileSync { get; set; }
     public bool AutoZip { get; set; }
+    public Dictionary<string, int> PackVersion { get; set; }
 }
