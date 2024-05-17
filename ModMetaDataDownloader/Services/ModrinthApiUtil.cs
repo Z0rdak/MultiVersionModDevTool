ae530bcca3b8824f8f5bf2032cf25495a2f12e4a
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-05-17 15:55:43 +0200

    add modrinth api util and invocable for scheduling for modrinth

---
+++ ModMetaDataDownloader/Services/ModrinthApiUtil.cs
@@ -0,0 +1,40 @@
+﻿using ModMetaDataDownloader.Model;
+using RestSharp;
+
+namespace ModMetaDataDownloader.Services;
+
+public class ModrinthApiUtil
+{
+    private readonly RestClient _restClient;
+    private readonly ILogger<ModrinthApiUtil> _logger;
+    private readonly ModrinthConfig _modrinthConfig;
+
+    public ModrinthApiUtil(IConfiguration configuration, ILogger<ModrinthApiUtil> logger)
+    {
+        
+        _logger = logger;
+        _modrinthConfig = configuration.GetSection("ModrinthApi").Get<ModrinthConfig>();
+        _restClient = new RestClient(_modrinthConfig.Host);
+    }
+
+
+    public async Task<List<ModFileInfo>> GetModFileInfo(ModrinthModInfo mod)
+    {
+        var requestTime = DateTimeOffset.UtcNow.ToUniversalTime();
+        var requestTimeStr = requestTime.ToString("yyyy'-'MM'-'dd'_'HH'-'mm'-'ss'-'fff'Z'");
+        
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("[{Time}] Getting metadata from modrinth...", requestTimeStr);
+        var request = new RestRequest($"/project/{mod.Id}/version");
+        var response = await _restClient.GetAsync<List<ModFileInfo>>(request);
+        if (response == null)
+        {
+            _logger.LogWarning("Error getting metadata at:  {time}", DateTimeOffset.UtcNow.ToUniversalTime()
+                .ToString("yyyy'-'MM'-'dd'_'HH'-'mm'-'ss'-'fff'Z'"));
+            return new List<ModFileInfo>();
+        }
+
+        return response;
+    }
+    
+}
---
+++ ModMetaDataDownloader/Services/WriteModrinthModFileInfo.cs
@@ -0,0 +1,96 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using Coravel.Invocable;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+using ModMetaDataDownloader.Model;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+namespace ModMetaDataDownloader.Services;
+
+public class WriteModrinthModFileInfo : IInvocable
+{
+    private readonly InfluxDbService _influxDbService;
+    private readonly ILogger<WriteModrinthModFileInfo> _logger;
+    private readonly ModrinthApiUtil _modrinthApi;
+    private readonly ModrinthConfig _modrinthConfig;
+
+    public WriteModrinthModFileInfo(IConfiguration configuration, InfluxDbService influxDbService, 
+        ModrinthApiUtil modrinthApi,
+        ILogger<WriteModrinthModFileInfo> logger)
+    {
+        _influxDbService = influxDbService;
+        _modrinthApi = modrinthApi;
+        _modrinthConfig =  _modrinthConfig = configuration.GetSection("ModrinthApi").Get<ModrinthConfig>();
+        _logger = logger;
+    }
+
+    public async Task Invoke()
+    {
+        var requestTime = DateTimeOffset.UtcNow.ToUniversalTime();
+        requestTime = requestTime.AddSeconds(-requestTime.Second);
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("[{Time}] Getting data from modrinth...", requestTime.ToString());
+
+        foreach (var mod in _modrinthConfig.Mods.Values)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+                _logger.LogInformation(
+                    "[{Time}] Getting mod download stats for '{ModName}' (with Id={ModId}) from modrinth...",
+                    requestTime.ToString(), mod.Slug, mod.Id);
+            var modFileInfos = await _modrinthApi.GetModFileInfo(mod);
+            var bucket = _influxDbService.InfluxConfig.Buckets[mod.Slug];
+            _logger.LogInformation("Writing modrinth download counts for '{modName}' to bucket '{bucket}'...", bucket.desc, bucket.name);
+            modFileInfos.ForEach(mfi =>
+            {
+                var bucket = _influxDbService.InfluxConfig.Buckets[mod.Slug];
+                _influxDbService.Write(writeApi =>
+                {
+                    // TODO: FIXME: could be simplified by restricting reading of newer versions which have a uniform naming convention
+                    var tokens = mfi.name.Split('-');
+                    var pattern = "";
+                    if (tokens.Length == 4)
+                    {
+                        pattern = @"^(?<mcversion>[^-]+)-(?<modversion>.+)-(?<modloader>[^-]+)$";
+                    }
+
+                    if (tokens.Length == 3)
+                    {
+                        pattern = @"^(?<mcversion>[^-]+)-(?<modversion>.+)$";
+                    }
+
+                    var match = Regex.Match(mfi.name, pattern);
+                    if (match.Success)
+                    {
+                        var modVersion = match.Groups["modversion"].Value;
+                        var modLoader = match.Groups["modloader"].Value;
+                        var mcVersion = match.Groups["mcversion"].Value;
+                        if (tokens.Length == 3)
+                        {
+                            // fallback for old versions
+                            modLoader = "forge";
+                        }
+                        
+                        var modloaders = mfi.loaders.Select(e => e.ToLower());
+                        var downloadCount = new ModrinthDownload()
+                        {
+                            FileId = mfi.id,
+                            FileName = mfi.name,
+                            ModVersion = modVersion, // mfi.version_number, TODO: version number could only be used for newer versions, since older dont have a uniform naming
+                            ModLoader = modLoader.ToLower(),
+                            ModLoaders = string.Join(',', modloaders),
+                            MinecraftVersion = mcVersion,
+                            MinecraftVersions = string.Join(',', mfi.game_versions),
+                            Count = mfi.downloads,
+                            ReleaseType = mfi.version_type.ToLower(),
+                            ReleaseDate = DateTime.Parse(mfi.date_published).ToUniversalTime().ToString(CultureInfo.CurrentCulture),
+                            Timestamp = requestTime
+                        };
+                        writeApi.WriteMeasurement(downloadCount, WritePrecision.S, bucket.name,
+                            _influxDbService.InfluxConfig.Org);
+                    }
+                });
+            });
+        }
+    }
+}
